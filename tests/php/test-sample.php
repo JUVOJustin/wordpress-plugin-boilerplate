6f8c35131e65a5517854ed7e8ff90121f86072b5
Justin Vogt <mail@justin-vogt.de>
2022-11-09 01:05:45 +0100

    Create test-sample.php

---
+++ tests/php/test-sample.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Demo_Plugin\Tests;
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends \WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
