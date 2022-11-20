ef3daffb2245841cb87abb4fd522491ae09de4bf
Justin Vogt <mail@justin-vogt.de>
2022-11-20 22:57:58 +0100

    Update webpack.mix.js

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -2,10 +2,12 @@ let mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 require('laravel-mix-merge-manifest');
 
-mix.js('frontend/js/demo-plugin-public.js', 'frontend/dist/js')
-   .sass('frontend/scss/demo-plugin-public.scss', 'frontend/dist/css')
+mix.js('frontend/js/demo-plugin-public.js', 'js')
+   .sass('frontend/scss/demo-plugin-public.scss', 'css')
    .purgeCss({
       content: ['frontend/views/**/*.twig'],
       css: ['frontend/dist/**/*.css']
   })
+  .setPublicPath('frontend/dist')
+  .setResourceRoot('../')
   .mergeManifest();
