b4258ba950eaf93b921b154b210a8b40d42c99aa
Justin Vogt <mail@justin-vogt.de>
2023-03-28 03:30:10 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   fileServerFolder: 'tests/cypress',
   fixturesFolder: 'tests/cypress/fixtures',
   screenshotsFolder: 'tests/cypress/screenshots',
