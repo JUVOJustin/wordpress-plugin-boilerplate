399b1589fbab92d785c8f3c6aad111f624de47ea
Justin Vogt <mail@justin-vogt.de>
2024-02-15 21:28:44 +0100

    Update headers

--- demo-plugin.php
+++ demo-plugin.php
@@ -14,11 +14,9 @@
  *
  * @wordpress-plugin
  * Plugin Name:       Demo Plugin
- * Plugin URI:        https://juvo-design.de
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Justin Vogt
- * Author URI:        https://juvo-design.de
+ * Requires PHP:      7.4
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       demo-plugin
