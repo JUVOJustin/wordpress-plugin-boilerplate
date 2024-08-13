75dc879d55bfbf1cc0bf7c1782d7ba815880ff48
Justin Vogt <mail@justin-vogt.de>
2024-08-13 14:20:53 +0200

    Downgrade to php8.0

--- demo-plugin.php
+++ demo-plugin.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Demo Plugin
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Requires PHP:      7.4
+ * Requires PHP:      8.0
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       demo-plugin
