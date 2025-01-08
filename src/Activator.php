93cab02c31293261866935d0e510496766924c17
Justin Vogt <mail@justin-vogt.de>
2025-01-08 17:55:26 +0100

    Add network activation logic for the plugin (#126)
    * Add network activation logic for the plugin
    
    Introduced a `network_activation` method in the `Activator` class to handle plugin activation across network sites. Linked this method to the `activated_plugin` action to ensure proper handling of network-wide activations.
    
    * Delete
    
    * Add .ddev to .gitignore
    
    Included .ddev directory in .gitignore to prevent it from being tracked by Git. This ensures consistency and avoids unintended changes to development environment configuration files.
    
    * Standardize code formatting and update comments.
    
    Aligned code to adhere to WordPress coding standards, including spacing and array syntax. Added clarity by improving comment formatting and disabling PHPCS warnings where necessary.
    
    * Update type-fest dependency to version 4.31.0
    
    Replaced type-fest version 0.12.0 with 4.31.0 and updated package-lock.json accordingly. Adjusted node engine requirement to ">=16" and marked the dependency as optional and peer. Includes a new type-fest entry specific to "ink" dependencies.
    
    * Update Babel dependencies to latest versions
    
    Upgraded various Babel packages in package-lock.json to their latest versions for improved functionality, bug fixes, and compatibility. This ensures the project aligns with the most recent updates in the Babel ecosystem.
    
    * npm install again

--- src/Activator.php
+++ src/Activator.php
@@ -24,4 +24,28 @@ class Activator {
 	 */
 	public static function activate(): void {
 	}
+
+	/**
+	 * Add logic to the activation on a network site.
+	 *
+	 * @param string $plugin Plugin file loaded.
+	 * @param bool   $network_wide Indicates if loaded network wide.
+	 * @return void
+	 */
+	public static function network_activation( string $plugin, bool $network_wide ): void {
+
+		if ( ! str_contains( $plugin, Demo_Plugin::PLUGIN_NAME ) || ! $network_wide ) {
+			return;
+		}
+
+		// phpcs:disable Squiz.PHP.CommentedOutCode.Found
+
+		// Network deactivate
+		// deactivate_plugins( $plugin, false, true );
+
+		// Activate on single site
+		// activate_plugins( $plugin );
+
+		// phpcs:enable
+	}
 }
