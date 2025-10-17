7493ed676e3e6336e569adfc91c29b4dfd342c35
Justin Vogt <mail@justin-vogt.de>
2025-10-17 20:35:10 +0200

    formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,16 @@
-const path = require('path');
+const path = require( 'path' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
-
 module.exports = {
-    ...defaultConfig,
-    entry: {
-        "demo-plugin-frontend": [
-            path.resolve(__dirname, 'resources/frontend/js/app.js'),
-            path.resolve(__dirname, 'resources/frontend/scss/app.scss'),
-        ],
-        "demo-plugin-admin": [
-            path.resolve(__dirname, 'resources/admin/js/app.js'),
-            path.resolve(__dirname, 'resources/admin/scss/app.scss'),
-        ]
-    }
+	...defaultConfig,
+	entry: {
+		'demo-plugin-frontend': [
+			path.resolve( __dirname, 'resources/frontend/js/app.js' ),
+			path.resolve( __dirname, 'resources/frontend/scss/app.scss' ),
+		],
+		'demo-plugin-admin': [
+			path.resolve( __dirname, 'resources/admin/js/app.js' ),
+			path.resolve( __dirname, 'resources/admin/scss/app.scss' ),
+		],
+	},
 };
